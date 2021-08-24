b98f8b78a3bdd33617e6f58c4743818681681bb8
Pajables <mattnel@outlook.com>
2021-08-24 15:04:45 +0200

    Fixed arduino file

--- Fluidic_backbone_arduino/Fluidic_backbone_arduino.ino
+++ Fluidic_backbone_arduino/Fluidic_backbone_arduino.ino
@@ -1,7 +1,7 @@
 /*
- Name:		Fluidic_backbone_arduino.ino
- Created:	2/1/2021 2:12:29 PM
- Author:	mattn
+ Name:    Fluidic_backbone_arduino.ino
+ Created: 2/1/2021 2:12:29 PM
+ Author:  mattn
 */
 
 //include core libraries
@@ -18,8 +18,7 @@ CommandManager cmdMng;
 #include <CommandAccelStepper.h>
 #include <CommandLinearAccelStepperActuator.h>
 #include <CommandDigitalWrite.h>
-<<<<<<< HEAD
-//#include <CommandDigitalRead.h>
+#include <CommandDigitalRead.h>
 
 //initialised module objects
 AccelStepper stpx(AccelStepper::DRIVER, 54, 55);
@@ -35,44 +34,12 @@ AccelStepper stpe0(AccelStepper::DRIVER, 26, 28);
 CommandAccelStepper cmdStpe0(stpe0, 24);
 
 AccelStepper stpe1(AccelStepper::DRIVER, 36, 34);
-CommandAccelStepper cmdStpe1(stpe1, 30);
-=======
-#include <CommandDigitalRead.h>
-
-//initialised module objects
-AccelStepper stpx(AccelStepper::DRIVER, 54, 55);
-CommandLinearAccelStepperActuator cmdStpx(stpx, 3);
-
-AccelStepper stpy(AccelStepper::DRIVER, 60, 61);
-CommandLinearAccelStepperActuator cmdStpy(stpy, 2);
-
-AccelStepper stpz(AccelStepper::DRIVER, 46, 48);
-CommandAccelStepper cmdStpz(stpz);
-
-AccelStepper stpe0(AccelStepper::DRIVER, 26, 28);
-CommandAccelStepper cmdStpe0(stpe0);
->>>>>>> 8b5369b3a1713a25e9057e6ee629d00983291042
-
-//enable pins
-CommandDigitalWrite enx(38);
-CommandDigitalWrite eny(56);
-CommandDigitalWrite enz(62);
-CommandDigitalWrite ene0(24);
-<<<<<<< HEAD
-CommandDigitalWrite ene1(30);
-=======
->>>>>>> 8b5369b3a1713a25e9057e6ee629d00983291042
+CommandAccelStepper cmdStpe1(stpe1, 30); 
 
 CommandAnalogRead ar1(A3);
 CommandAnalogRead ar2(A4);
 
 CommandAnalogRead t1(A13);
-<<<<<<< HEAD
-
-//PWM pins
-CommandAnalogWrite aw1(8);
-CommandAnalogWrite aw2(9);
-=======
 CommandAnalogRead t2(A14);
 
 //PWM pins
@@ -87,53 +54,31 @@ CommandDigitalRead dr2(59);
 CommandDigitalWrite dw1(16);
 CommandDigitalWrite dw2(17);
 
->>>>>>> 8b5369b3a1713a25e9057e6ee629d00983291042
 
 void setup() {
-	//register modules with commandmanager
-	Serial.begin(115200);
-	cmdStpx.registerToCommandManager(cmdMng, "STPX");
-	cmdStpy.registerToCommandManager(cmdMng, "STPY");
-	cmdStpz.registerToCommandManager(cmdMng, "STPZ");
-	cmdStpe0.registerToCommandManager(cmdMng, "STPE0");
-<<<<<<< HEAD
+  //register modules with commandmanager
+  Serial.begin(115200);
+  cmdStpx.registerToCommandManager(cmdMng, "STPX");
+  cmdStpy.registerToCommandManager(cmdMng, "STPY");
+  cmdStpz.registerToCommandManager(cmdMng, "STPZ");
+  cmdStpe0.registerToCommandManager(cmdMng, "STPE0");
   cmdStpe1.registerToCommandManager(cmdMng, "STPE1");
-=======
->>>>>>> 8b5369b3a1713a25e9057e6ee629d00983291042
 
-	enx.registerToCommandManager(cmdMng, "ENX");
-	eny.registerToCommandManager(cmdMng, "ENY");
-	enz.registerToCommandManager(cmdMng, "ENZ");
-	ene0.registerToCommandManager(cmdMng, "ENE0");
-<<<<<<< HEAD
-  ene1.registerToCommandManager(cmdMng, "ENE1");
-=======
->>>>>>> 8b5369b3a1713a25e9057e6ee629d00983291042
+  ar1.registerToCommandManager(cmdMng, "AR1");
+  ar2.registerToCommandManager(cmdMng, "AR2");
 
-	ar1.registerToCommandManager(cmdMng, "AR1");
-	ar2.registerToCommandManager(cmdMng, "AR2");
+  t1.registerToCommandManager(cmdMng, "T1");
+  t2.registerToCommandManager(cmdMng, "T2");
 
-	t1.registerToCommandManager(cmdMng, "T1");
-<<<<<<< HEAD
+  aw1.registerToCommandManager(cmdMng, "AW1");
+  aw2.registerToCommandManager(cmdMng, "AW2");
 
-	aw1.registerToCommandManager(cmdMng, "AW1");
-	aw2.registerToCommandManager(cmdMng, "AW2");
-=======
-	t2.registerToCommandManager(cmdMng, "T2");
+  dr1.registerToCommandManager(cmdMng, "DR1");
+  dr2.registerToCommandManager(cmdMng, "DR2");
 
-	aw1.registerToCommandManager(cmdMng, "STIR");
-	aw2.registerToCommandManager(cmdMng, "HCAP");
-
-	dr1.registerToCommandManager(cmdMng, "DR1");
-	dr2.registerToCommandManager(cmdMng, "DR2");
-
-	dw1.registerToCommandManager(cmdMng, "DW1");
-	dw2.registerToCommandManager(cmdMng, "DW2");
->>>>>>> 8b5369b3a1713a25e9057e6ee629d00983291042
-
-	cmdMng.init();
+  cmdMng.init();
 }
 
 void loop() {
-	cmdMng.update();
+  cmdMng.update();
 }
